f89a90bb5b7c13ffd1236f5098cf168589fff050
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2021-07-18 20:53:19 -0700

    Se agrego un nav para el proceso

---
+++ src/components/Generales/Navigation.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from 'react' 
+import { Navbar,Nav,NavDropdown,Image } from 'react-bootstrap'
+// import logo from '../images/hemolife.png'
+import { Link } from 'react-router-dom'
+
+//const Navigation = ( expedienteActivo, admisionActivo ) => {
+class Navigation extends Component {
+    render() {
+        return(
+            <Navbar bg="dark" variant="dark" sticky="top" expand="lg">
+                {/* <Navbar.Brand as={Link} to="/">
+                    <img src={logo} width="150" height="30" alt="Hemolife"/>
+                </Navbar.Brand> */}
+                <Navbar.Brand href="#home">
+                    Capacisoft
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Catalogos" id="collasible-nav-dropdown">  
+                            <NavDropdown.Item as={Link} to="#action/1.1">Usuarios</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/1.2">Universidades</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/1.3">Carreras</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/1.4">Alumnos</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/1.5">Proveedores</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/1.6">Tema de Curso</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/1.5">Nombre del Curso</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/1.6">Periodo</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Procesos" id="collasible-nav-dropdown">  
+                            <NavDropdown.Item as={Link} to="#action/3.1">Ver perfil</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/3.2">Ajustes</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={Link} to="#action/3.4">Cerrar sesion</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Reportes" id="collasible-nav-dropdown" >  
+                            <NavDropdown.Item as={Link} to="#action/3.1">Ver perfil</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="#action/3.2">Ajustes</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={Link} to="#action/3.4">Cerrar sesion</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link >Usuario</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            
+        )
+    }
+}
+
+export default Navigation
