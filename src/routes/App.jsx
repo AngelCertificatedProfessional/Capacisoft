0ded901ddda410c709021b2bd84703ac6909fc2e
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2021-08-12 00:02:57 -0700

    Se quitaron lienas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { BrowserRouter,Switch,Route } from 'react-router-dom'
-// import Layout from '../components/Generales/Layout';
-import Home from './../containers/Home'
-// import Universidad from '../containers/Universidad'
-// import Carrera from '../containers/Carrera'
-// import TemaCurso from '../containers/TemaCurso'
-// import Usuario from '../containers/Usuario'
-// import Alumno from '../containers/Alumno'
-// import Curso from '../containers/Curso'
-// import Periodo from '../containers/Periodo'
-// import Login from '../containers/Login'
-import AppConText from './../context/AppContext';
-import useInitialState from './../hooks/useInitialState';
+import Layout from '../components/Generales/Layout';
+import Home from '../containers/Home'
+import Universidad from '../containers/Universidad'
+import Carrera from '../containers/Carrera'
+import TemaCurso from '../containers/TemaCurso'
+import Usuario from '../containers/Usuario'
+import Alumno from '../containers/Alumno'
+import Curso from '../containers/Curso'
+import Periodo from '../containers/Periodo'
+import Login from '../containers/Login'
+import AppConText from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
 const App = () => {
     const initialState = useInitialState();
@@ -19,19 +19,19 @@ const App = () => {
     return (
         <AppConText.Provider value={initialState}>
             <BrowserRouter>
-                {/* <Layout> */}
+                <Layout>
                     <Switch>
                         <Route exact path="/" component={Home}/>
-                        {/* <Route exact path="/universidad" component={Universidad}/>
+                        <Route exact path="/universidad" component={Universidad}/>
                         <Route exact path="/carerra" component={Carrera}/>
                         <Route exact path="/temaCurso" component={TemaCurso}/>
                         <Route exact path="/usuario" component={Usuario}/>
                         <Route exact path="/alumno" component={Alumno}/>
                         <Route exact path="/curso" component={Curso}/>
                         <Route exact path="/periodo" component={Periodo}/>
-                        <Route exact path="/login" component={Login}/> */}
+                        <Route exact path="/login" component={Login}/>
                     </Switch>
-                {/* </Layout> */}
+                </Layout>
             </BrowserRouter>
         </AppConText.Provider>
     );
