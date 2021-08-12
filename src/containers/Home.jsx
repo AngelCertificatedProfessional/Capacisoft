29ff5b62460447287480d1fb0c71e12343931e24
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2021-08-11 23:34:23 -0700

    Se comentarion lineas de lazy

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,23 +1,6 @@
-import React,{useEffect,useContext }  from 'react'
-import AppContext from '../context/AppContext';
-import {validaUsuario} from '../utils/UtileriasPagina'
-import { useHistory } from "react-router-dom";
+import React from 'react'
 
 const Home = () => {
-
-    const { state, agregarUsuario } = useContext(AppContext);
-    let history = useHistory();
-    // const { usuario } = state;
-    // console.log(state);
-    useEffect (() => {
-        // Buffer.from("SGVsbG8gV29ybGQ=", 'base64').toString('ascii')
-        //console.log(localStorage.getItem("usuario") );
-        // console.log(usuario)
-        // if(!validaUsuario(state,agregarUsuario)){
-        //     history.push('/login');
-        // }
-    }, [] )
-
     return(
         <>
             <p>Hola</p>
